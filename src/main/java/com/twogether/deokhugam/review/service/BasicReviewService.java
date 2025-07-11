cfd873aeefb25bdfc17dbee943bff5ebe998df55
wangcoJiin <mf0d070208@gmail.com>
2025-07-11 15:48:00 +0900

    fix: 누락된 Transactional 어노테이션 추가

--- src/main/java/com/twogether/deokhugam/review/service/BasicReviewService.java
+++ src/main/java/com/twogether/deokhugam/review/service/BasicReviewService.java
@@ -14,6 +14,7 @@ import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,6 +28,7 @@ public class BasicReviewService implements ReviewService{
 
     // 리뷰 생성
     @Override
+    @Transactional
     public ReviewDto create(ReviewCreateRequest request) {
         // 이미 존재하는 리뷰 시 예외
         if (reviewRepository.existsByUserIdAndBookId(request.userId(), request.bookId())){
