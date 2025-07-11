09692546e7d07e92aea356cff43d5b91923f590a
okodee <okodee@naver.com>
2025-07-11 14:12:44 +0900

    feat: add S3 configuration

---
+++ src/main/java/com/twogether/deokhugam/config/S3Config.java
@@ -0,0 +1,34 @@
+package com.twogether.deokhugam.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    // .env 파일에서 AWS 관련 설정값 주입
+    @Value("${AWS_S3_ACCESS_KEY}")
+    private String accessKey;
+
+    @Value("${AWS_S3_SECRET_KEY}")
+    private String secretKey;
+
+    @Value("${AWS_S3_REGION}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+}
