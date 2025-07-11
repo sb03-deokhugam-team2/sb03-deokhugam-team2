feadb084800c9074b56e7f35f20c7a1ce46f7383
wangcoJiin <mf0d070208@gmail.com>
2025-07-11 15:45:17 +0900

    refactor: ReviewMapper 에서 Review, Book, User 객체의 null 안정성 체크

--- src/main/java/com/twogether/deokhugam/review/mapper/ReviewMapper.java
+++ src/main/java/com/twogether/deokhugam/review/mapper/ReviewMapper.java
@@ -1,19 +1,32 @@
 package com.twogether.deokhugam.review.mapper;
 
+import com.twogether.deokhugam.book.entity.Book;
 import com.twogether.deokhugam.review.dto.ReviewDto;
 import com.twogether.deokhugam.review.entity.Review;
+import com.twogether.deokhugam.user.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReviewMapper {
 
     public ReviewDto toDto(Review review){
+        if (review == null){
+            return null;
+        }
+
+        Book book = review.getBook();
+        User user = review.getUser();
+
+        if (book == null || user == null){
+            throw new IllegalStateException("Review가 null 값인 도서나 사용자를 참조하고 있습니다.");
+        }
+
         return new ReviewDto(
                 review.getId(),
-                review.getBook().getId(),
+                book.getId(),
                 review.getBookTitle(),
                 review.getBookThumbnailUrl(),
-                review.getUser().getId(),
+                user.getId(),
                 review.getUserNickName(),
                 review.getContent(),
                 review.getRating(),
