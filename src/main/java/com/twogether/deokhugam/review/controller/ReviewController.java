4caef43c2ad76b31715474b8d5244353f0f59731
wangcoJiin <mf0d070208@gmail.com>
2025-07-11 16:40:15 +0900

    refactor: API 경로에 leading slash 추가
    - REST API 경로는 일반적으로 leading slash로 시작하는 것이 관례

--- src/main/java/com/twogether/deokhugam/review/controller/ReviewController.java
+++ src/main/java/com/twogether/deokhugam/review/controller/ReviewController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RequiredArgsConstructor
-@RequestMapping("api/reviews")
+@RequestMapping("/api/reviews")
 @RestController
 public class ReviewController {
 
